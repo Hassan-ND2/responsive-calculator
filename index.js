4659430251c89d40e9e66ecf7f5bdcf0d40e9776
Hassan-ND2 <hassannasseraldeengithubnew@gmail.com>
2024-04-24 18:06:38 +0300

    responsive calculator usinfg html css javascript

---
+++ index.js
@@ -0,0 +1,28 @@
+
+    
+   
+
+    function addit(input){
+        const display =document.getElementById("display");
+        display.value+=input;
+    } 
+    
+    function deleteall (input){
+        const display=document.getElementById("display")
+        display.value='';
+
+
+    }
+
+    function addall(){
+        const display=document.getElementById("display");
+        display.value=eval(display.value);
+        
+        
+    }
+
+
+
+    
+
+
